4112ff30f35b026455b50f2a7f2d6158a1a6785f
zhuhongbo <1010724827@qq.com>
2023-04-24 14:07:14 +0800

    'accessing-data-mysql-init'

---
+++ accessing-data-mysql/README.md
@@ -0,0 +1,23 @@
+# 技术特点：JPA+Mysql。
+
+'@Controller,@Repository(Dao)，@Entity，挺实用的，
+
+## 注意点：
+
+spring.jpa.hibernate.ddl-auto生产环境为none，开发环境为update
+
+## 三种依赖Spring Web，Spring Data JPA和MySQL驱动程序。
+
+![img.png](img.png)
+
+## 建库
+
+![img_1.png](img_1.png)
+
+## 使用Postman创建用户
+
+![img_2.png](img_2.png)
+
+## 查询所有用户
+
+![img_3.png](img_3.png)
---
+++ accessing-data-mysql/src/main/java/com/example/accessingdatamysql/User.java
@@ -0,0 +1,46 @@
+package com.example.accessingdatamysql;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * @Entity 告诉hibernate用这个类去创建一个表
+ * TODO:某些项目会加入@Data，未见效果，不加也没事？
+ */
+@Entity // This tells Hibernate to make a table out of this class
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String name;
+
+    private String email;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
